d4c3c9b09dec1e00388e118aa82f810915aee9a0
Your Name <you@example.com>
2025-02-02 21:51:30 +0100

    .

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -11,6 +11,19 @@ return {
 	-- 	lazy = false, -- Ensure it loads automatically
 	-- },
 
+	{
+		"michaelrommel/nvim-silicon",
+		lazy = true,
+		cmd = "Silicon",
+		main = "nvim-silicon",
+		opts = {
+			-- Configuration here, or leave empty to use defaults
+			line_offset = function(args)
+				return args.line1
+			end,
+		},
+	},
+
 	-- barbra for the tabs when you open a buffer or something like that
 	{
 		"romgrk/barbar.nvim",
