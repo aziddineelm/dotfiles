ca2cdd922d2037d639b7e7ff470f81e2b97d4413
aziddineelm <azedine160@gmail.com>
2025-02-07 20:17:31 +0100

    .

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -15,11 +15,21 @@ return {
 		"folke/noice.nvim",
 		event = "VeryLazy",
 		opts = {
-			-- add any options here
+			-- Add the following section to silence notifications during cmdline usage
+			messages = {
+				enabled = false,
+				-- view = "mini",
+				-- view_error = "mini",
+				-- view_warn = "mini",
+				-- view_history = "messages",
+				-- view_search = false, -- Disable search notifications
+			},
+			notify = {
+				enabled = false, -- Disable notifications entirely
+			},
 		},
 		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries "MunifTanjim/nui.nvim",
 			-- OPTIONAL:
 			--   `nvim-notify` is only needed, if you want to use the notification view.
 			--   If not available, we use `mini` as the fallback
@@ -142,4 +152,26 @@ return {
 			return opts
 		end,
 	},
+
+	-- Setup cmdline completions
+	-- require("cmp").setup.cmdline(":", {
+	-- 	mapping = require("cmp").mapping.preset.cmdline(),
+	-- 	sources = require("cmp").config.sources({
+	-- 		{ name = "path" },
+	-- 	}, {
+	-- 		{
+	-- 			name = "cmdline",
+	-- 			option = {
+	-- 				ignore_cmds = { "Man", "!" },
+	-- 			},
+	-- 		},
+	-- 	}),
+	-- }),
+	--
+	-- require("cmp").setup.cmdline("/", {
+	-- 	mapping = require("cmp").mapping.preset.cmdline(),
+	-- 	sources = {
+	-- 		{ name = "buffer" },
+	-- 	},
+	-- }),
 }
