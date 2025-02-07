87a0d308ca2ace7e693810ac252345e5b5d6342d
aziddineelm <azedine160@gmail.com>
2025-02-07 12:01:42 +0100

    .

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -15,21 +15,18 @@ return {
 		"folke/noice.nvim",
 		event = "VeryLazy",
 		opts = {
-			routes = {
-				{
-					filter = { event = "notify", find = "No information available" },
-					opts = { skip = true },
-				},
-			},
-			presets = {
-				lsp_doc_border = true,
-			},
+			-- add any options here
 		},
 		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
 			"rcarriga/nvim-notify",
 		},
 	},
+
 	{
 		"michaelrommel/nvim-silicon",
 		lazy = true,
