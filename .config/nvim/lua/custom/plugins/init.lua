d31043921caf30e77914a3b5b31af4c676c355d1
aziddine <aziddine160@gmail.com>
2026-08-02 11:59:42 +0100

    "feat: Added Herder config. and added nvim plugin for md preview."

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -89,4 +89,20 @@ return {
 		},
 		version = "^1.0.0",
 	},
+
+	-- MarkDown Preview.
+	{
+		"selimacerbas/markdown-preview.nvim",
+		dependencies = { "selimacerbas/live-server.nvim" },
+		config = function()
+			require("markdown_preview").setup({
+				-- all optional; sane defaults shown
+				instance_mode = "takeover", -- "takeover" (one tab) or "multi" (tab per instance)
+				port = 0, -- 0 = auto (8421 for takeover, OS-assigned for multi)
+				open_browser = true,
+				default_theme = "dark", -- "dark" or "light"; initial preview theme
+				debounce_ms = 300,
+			})
+		end,
+	},
 }
